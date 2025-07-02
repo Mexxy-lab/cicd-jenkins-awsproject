531507ef7f4be3600039931112bced12a1adcba7
it-admins <it-admins@ectios.com>
2025-07-02 14:22:16 -0400

    updated files

--- userdata-cd/backend-stack.sh
+++ userdata-cd/backend-stack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DATABASE_PASS='admin123'
-yum update -y
+# yum update -y
 yum install epel-release -y
 yum install mariadb-server -y
 yum install wget git unzip -y
@@ -12,44 +12,71 @@ sed -i 's/^127.0.0.1/0.0.0.0/' /etc/my.cnf
 systemctl start mariadb
 systemctl enable mariadb
 
-#restore the dump file for the application
 cd /tmp/
-wget https://raw.githubusercontent.com/devopshydclub/vprofile-repo/vp-rem/src/main/resources/db_backup.sql
-mysqladmin -u root password "$DATABASE_PASS"
-mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
-mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
-mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
-mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
-mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
-mysql -u root -p"$DATABASE_PASS" -e "create database accounts"
-mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'localhost' identified by 'admin123'"
-mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'%' identified by 'admin123'"
-mysql -u root -p"$DATABASE_PASS" accounts < /tmp/db_backup.sql
-mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
+git clone -b main https://github.com/devopshydclub/vprofile-project.git
+#restore the dump file for the application
+sudo mysqladmin -u root password "$DATABASE_PASS"
+sudo mysql -u root -p"$DATABASE_PASS" -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DATABASE_PASS');"
+sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
+sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
+sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
+sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
+sudo mysql -u root -p"$DATABASE_PASS" -e "create database accounts"
+sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'localhost' identified by 'admin123'"
+sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'%' identified by 'admin123'"
+sudo mysql -u root -p"$DATABASE_PASS" accounts < /tmp/vprofile-project/src/main/resources/db_backup.sql
+sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
 
 # Restart mariadb-server
-systemctl restart mariadb
+sudo systemctl restart mariadb
+
+#starting the firewall and allowing the mariadb to access from port no. 3306
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+sudo firewall-cmd --zone=public --add-port=3306/tcp --permanent
+sudo firewall-cmd --reload
+sudo systemctl restart mariadb
+sudo systemctl status mariadb
+
 # SETUP MEMCACHE
-yum install memcached -y
-systemctl start memcached
-systemctl enable memcached
-systemctl status memcached
-memcached -p 11211 -U 11111 -u memcached -d
+sudo dnf install memcached -y
+sudo systemctl start memcached
+sudo systemctl enable memcached
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/sysconfig/memcached
+sudo systemctl restart memcached
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+sudo systemctl status firewalld
+firewall-cmd --add-port=11211/tcp
+firewall-cmd --runtime-to-permanent
+firewall-cmd --add-port=11111/udp
+firewall-cmd --runtime-to-permanent
+sudo systemctl restart firewalld
+sudo memcached -p 11211 -U 11111 -u memcached -d
+sudo systemctl restart firewalld
+sudo systemctl restart memcached
+sudo systemctl status memcached
+
+# Setup for Rabbitmq
 sleep 30
 yum install socat -y
-yum install wget -y
-wget https://www.rabbitmq.com/releases/rabbitmq-server/v3.6.10/rabbitmq-server-3.6.10-1.el7.noarch.rpm
-rpm --import https://www.rabbitmq.com/rabbitmq-release-signing-key.asc
-yum update
-rpm -Uvh rabbitmq-server-3.6.10-1.el7.noarch.rpm
-systemctl start rabbitmq-server
-systemctl enable rabbitmq-server
-systemctl status rabbitmq-server
+cd /tmp/
+dnf -y install centos-release-rabbitmq-38
+ dnf --enablerepo=centos-rabbitmq-38 -y install rabbitmq-server
+ systemctl enable --now rabbitmq-server
+ systemctl start firewalld 
+ systemctl enable firewalld
+ firewall-cmd --add-port=5672/tcp
+ firewall-cmd --runtime-to-permanent
+ systemctl restart firewalld
+sudo systemctl start rabbitmq-server
+sudo systemctl enable rabbitmq-server
 sudo sh -c 'echo "loopback_users = none" | sudo tee /etc/rabbitmq/rabbitmq.conf'
 sudo cat /etc/rabbitmq/rabbitmq.conf
-rabbitmqctl add_user test test
-rabbitmqctl set_user_tags test administrator
+sudo rabbitmqctl add_user test test
+sudo rabbitmqctl set_user_tags test administrator
 sudo rabbitmqctl set_permissions -p / test ".*" ".*" ".*"
 sudo rabbitmqctl delete_user guest
-systemctl restart rabbitmq-server
-
+sudo rabbitmqctl list_users
+sudo systemctl restart rabbitmq-server
+sudo systemctl status rabbitmq-server
